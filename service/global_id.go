c5bc68edec9b97e8623310e15ca65fb62ee2e723
thewindear <thewindear@Outlook.com>
2022-08-27 01:41:40 +0800

    [feat]调整传参

--- service/global_id.go
+++ service/global_id.go
@@ -1,42 +1,47 @@
 package service
 
 import (
-    "time"
+	"context"
+	"github.com/go-redis/redis/v9"
+	"time"
 )
 
 // GlobalId 生成全局唯一id的结构体
 type GlobalId struct {
-    // 生成的id的总位数
-    CountBit uint
-    // 生成id的开始时间
-    BeginTime int64
+	// 生成的id的总位数
+	CountBit uint
+	// 生成id的开始时间
+	BeginTime int64
+	// redis
+	rdb *redis.Client
 }
 
 // NewGlobalId 实例化
-func NewGlobalId(countBit uint) *GlobalId {
-    //指定一个开始时间
-    beginTime := time.Date(2022, 8, 1, 0, 0, 0, 0, time.Local)
-    return &GlobalId{
-        CountBit:  countBit,
-        BeginTime: beginTime.Unix(),
-    }
+func NewGlobalId(countBit uint, rdb *redis.Client) *GlobalId {
+	//指定一个开始时间
+	beginTime := time.Date(2022, 8, 1, 0, 0, 0, 0, time.Local)
+	return &GlobalId{
+		CountBit:  countBit,
+		BeginTime: beginTime.Unix(),
+		rdb:       rdb,
+	}
 }
 
 // CreateId 生成全局唯一id实现方法
 // @param keyPrefix 用于区分业务
-func (g *GlobalId) CreateId(keyPrefix string) (int64, error) {
-    // 1. 生成时间戳
-    timestamp := time.Now().Unix() - g.BeginTime
-    // 2. 生成序列号
-    // 2.1 这里使用当天日期作为辅助id
-    nowDate := time.Now().Format("20060102")
-    key := "icr:" + keyPrefix + ":" + nowDate
-    count, err := rdb.Incr(ctx, key).Result()
-    if err != nil {
-        return 0, err
-    }
-    // 3. 拼接返回
-    // 3.1 按位运算进行拼接
-    // 3.2 时间戳左移 countBit位
-    return timestamp<<g.CountBit | count, err
+func (g *GlobalId) CreateId(ctx context.Context, keyPrefix string) (int64, error) {
+	// 1. 生成时间戳
+	timestamp := time.Now().Unix() - g.BeginTime
+	// 2. 生成序列号
+	// 2.1 这里使用当天日期作为辅助id
+	nowDate := time.Now().Format("20060102")
+	key := "icr:" + keyPrefix + ":" + nowDate
+	count, err := g.rdb.Incr(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+	// 3. 拼接返回
+	// 3.1 按位运算进行拼接
+	// 3.2 时间戳左移 countBit位
+	return timestamp<<g.CountBit | count, err
 }
--- service/global_id_test.go
+++ service/global_id_test.go
@@ -1,16 +1,31 @@
 package service
 
 import (
-	"testing"
+    "sync"
+    "testing"
 )
 
 func TestRedisIdWorker(t *testing.T) {
-	global := NewGlobalId(32)
-	id, err := global.CreateId("order")
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Logf("当前生成的id: %d", id)
-	}
+    global := NewGlobalId(32, rdb)
+    id, err := global.CreateId(ctx, "order")
+    if err != nil {
+        t.Error(err)
+    } else {
+        t.Logf("当前生成的id: %d", id)
+    }
+}
 
+func TestConcurrentRedisIdWork(t *testing.T) {
+    global := NewGlobalId(32, rdb)
+    wg := sync.WaitGroup{}
+    wg.Add(500)
+    for i := 0; i < 500; i++ {
+        go func() {
+            id, _ := global.CreateId(ctx, "order")
+            t.Logf("id: %d", id)
+            wg.Done()
+        }()
+    }
+    wg.Wait()
+    t.Log("test done")
 }
